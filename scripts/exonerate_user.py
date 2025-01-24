3be8e69d32ae3ef13a1a38627a2ce20de478a6f3
Brady Spiva <bspiva@tines.io>
2025-01-24 12:32:19 -0600

    add project files

--- scripts/exonerate_user.py
+++ scripts/exonerate_user.py
@@ -1,4 +1,5 @@
 # a script to return a user's access back to what it was prior to a quarantine
 
+
 if __name__ == "__main__":
     print("user has been exonerated... but we're watching you...")
---
+++ testing-github-deps.py
@@ -0,0 +1,9 @@
+# /// script
+# dependencies = [
+#   "test-python@git+https://github.com/brady-spiva/test-python.git"
+# ]
+# ///
+
+from test_python import scripts
+
+scripts.quarantine_user.main()
