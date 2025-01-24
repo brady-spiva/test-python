377b6f26fa0d089d604035c67de7bede82f4789f
Brady Spiva <bspiva@tines.io>
2025-01-24 08:03:00 -0600

    adding phony scripts

---
+++ scripts/nuke_user.py
@@ -0,0 +1,4 @@
+# a script to completely remove a user's access from systems. Use with care
+
+if __name__ == "__main__":
+    print("user has been nuked")
---
+++ scripts/quarantine_user.py
@@ -0,0 +1,4 @@
+# a script to restrict a user's access for a specified period of time
+
+if __name__ == "__main__":
+    print("user has been quarantined")
